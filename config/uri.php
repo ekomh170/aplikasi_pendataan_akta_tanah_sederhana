0ae095f7fb08210feae0f29fe56e84af516e2499
ekomh170 <ekomh020@gmail.com>
2021-10-01 22:12:55 +0700

    memperbarui config/uri.php | config buatan

--- config/uri.php
+++ config/uri.php
@@ -2,6 +2,6 @@
 
 function base_url()
 {
-    $base_url = "http://localhost:8080/github/app-tanah-ku/"; // disesuaikan sesuai direktori root dokumen
+    $base_url = "http://localhost:8080/github/aplikasi_pendataan_akta_tanah_sederhana/"; // disesuaikan sesuai direktori root dokumen
     return $base_url;
 }
